59f43b381a229272945320e1eef7bca4ce76682f
mannataneja <tt3092@g.rit.edu>
2025-01-21 13:39:22 -0500

    Basic shop scene created

---
+++ Assets/Scripts/ShopManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ShopManager : MonoBehaviour
+{
+    public GameObject[] items;
+    public GameObject[] plantPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShufflePlants();
+        FillShop();
+    }
+    void ShufflePlants()
+    {
+        for (int i = 0; i < plantPrefabs.Length; i++)
+        {
+            GameObject tmp = plantPrefabs[i];
+            int r = Random.Range(i, plantPrefabs.Length);
+            plantPrefabs[i] = plantPrefabs[r];
+            plantPrefabs[r] = tmp;
+        }
+    }
+    void FillShop()
+    {
+        for(int i = 0; i < items.Length; i++)
+        {
+            Instantiate(plantPrefabs[i], items[i].transform);
+            items[i].transform.GetChild(0).GetComponentInChildren<TMP_Text>().text = plantPrefabs[i].name;
+        }
+    }
+    public void BuyPlant(int itemIndex)
+    {
+        string itemName = transform.GetChild(itemIndex - 1).GetChild(1).name;
+        Debug.Log("Buy Item " + itemIndex + ", Plant Name : " + itemName);
+    }
+}
