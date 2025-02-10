46cf6e40350add6873fed17d8b55e60c3185c798
Matt Izzo (RIT Student) <mmi4105@rit.edu>
2025-02-10 13:28:44 -0500

    Menu functionality almost done
    Pushing to save these for later

---
+++ Assets/Scripts/UIScripts/Pause.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    private bool paused;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(timer <= 0)
+        {
+            timer = 1;
+            Pause();
+        }
+        timer -= Time.deltaTime;*/
+
+    }
+
+    public void TogglePause()
+    {
+        paused = !paused;
+
+        if (paused)
+        {
+            pauseMenu.SetActive(true);
+        }
+        else
+        {
+            pauseMenu.SetActive(false);
+        }
+    }
+}
