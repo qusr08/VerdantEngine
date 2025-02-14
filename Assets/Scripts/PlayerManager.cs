8ec91fbc251fa8b957258c92b36af0a63bd1a48b
Matt Izzo (RIT Student) <mmi4105@rit.edu>
2025-02-14 11:10:04 -0500

    I simply don't know what data type inventory should be :P
    Money and garden are good?

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,7 +7,9 @@ public class PlayerManager : MonoBehaviour
 
     public static PlayerManager playerManager;
 
-
+    public int money;
+    [SerializeField] private PlayerData _playerData;
+    public PlayerData PlayerData => _playerData;
 
     // Start is called before the first frame update
     void Start()
