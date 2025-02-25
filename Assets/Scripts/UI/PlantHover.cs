fcef31a6a3bde6c02424527bf12497b4293fc462
Frank Alfano <q24xboss@gmail.com>
2025-02-25 10:37:58 -0500

    F_GardenUI merge fixes

--- Assets/Scripts/UI/PlantHover.cs
+++ Assets/Scripts/UI/PlantHover.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlantHover : MonoBehaviour {
 	[SerializeField] private TMP_Text plantName;
 	[SerializeField] private TMP_Text plantDescription;
 	[SerializeField] private TMP_Text plantHP;
+	[SerializeField] private Image plantImage;
 
-	public void UpdateText (string name, string description, Stat HealthStat) {
+	public void UpdateText (string name, string description, int currentHealth, int maxHealth, Sprite image) {
 		plantName.text = name;
 		plantDescription.text = description;
-		plantHP.text = "" + HealthStat.MaxValue + "/" + HealthStat.CurrentValue;
+		plantHP.text = "" + currentHealth + "/" + maxHealth;
+		plantImage.sprite = image;
 	}
 }
